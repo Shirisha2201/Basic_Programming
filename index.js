d883cad901e2490c465dec7c1996a26a1a07ff23
Shirisha <shirishabaddal2201@gmail.com>
2025-11-13 22:17:18 +0530

    Added evenOdd function

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-function isPrime(num){
-    if(num<=1) return false;
-    for(let i=2; i<=Math.sqrt(num); i++){
-        if(num%i===0) return false;
-    }
-    return true;
+function evenOdd(num){
+    return num%2===0;
 }
-console.log(isPrime(7));
-console.log(isPrime(15));
+console.log(evenOdd(4));
+console.log(evenOdd(7));
